079c78afb1382bb7f5d1a55d3dbbd362bf36f474
snowan <xiaowei.wan89@gmail.com>
2024-02-15 12:11:12 -0800

    load balancer step3 add health check

--- load_balancer/backend_server_5002.py
+++ load_balancer/backend_server_5002.py
@@ -8,5 +8,10 @@ def hello():
     print(f"hello from backend server 5002")
     return 'Hello from Backend Server 5002!'
 
+@app.route('/health')
+def healthy():
+    print("health check OK")
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(port=5002)
